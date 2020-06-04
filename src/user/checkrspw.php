7110e1344eaddd75998bd33b52d98cf3722b7265
Steven <qazzaqqaz71@mail.fcu.edu.tw>
2020-06-04 11:11:20 +0800

    update

--- src/user/checkrspw.php
+++ src/user/checkrspw.php
@@ -6,6 +6,8 @@ $db = Database::get();
 $password = $_POST["password"];
 $check = $_POST["checkpass"];
 if (!strcmp($password, $check)) {
+    $result = $db->execute("SELECT * FROM USERNAME WHERE username = ? AND password = ?;", array($username, $password));
+
     echo "密碼已重設";
 } else {
     echo "密碼不一致";
