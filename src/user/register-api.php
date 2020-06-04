0eed1c967b1a3c6faac62cdba29be72930d19c6f
TimChao <chao860204@gmail.com>
2020-06-04 14:37:01 +0800

    註冊

--- src/user/register-api.php
+++ src/user/register-api.php
@@ -1,6 +1,6 @@
 <?php
   require __DIR__ . '/../vendor/autoload.php';
-  
+
   // 建立 Session
   session_start();
 
@@ -18,15 +18,14 @@
   $university = $_POST['university'];
   $major = $_POST['major'];
   $stdId = $_POST['stdId'];
-  $stdId_img = $_POST['stdId_img'];
   
   //判斷是否填寫
   if($name == "" || $password == "") {
     echo "<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."請填寫完成！"."\"".")".";"."</script>";
-    echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";    
+    echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";
     exit;
   }
-  
+
   //驗證資料格式
   if(preg_match('/^[A-Za-z0-9]*[A-Z]+[A-Za-z0-9]*[0-9]+[A-Za-z0-9]*$/', $password)) {
      if(preg_match('/^09[0-9]{8}$/' , $phonenumber)) {
@@ -34,31 +33,35 @@
           if(preg_match('/^[A-Za-z0-9]*[A-Z]+[A-Za-z0-9]*[0-9]+[A-Za-z0-9]*$/',$stdId)) {
             if($gender == '男' || $gender == '女' ) {
               if(preg_match("/^[\x{4e00}-\x{9fa5}]+$/u",$name)) {
+                if(in_array($extension,array('jpg','png','gif'))) {
                 $repeat_email = $db->execute("SELECT * FROM generaluser WHERE email = ? ;", array($email));
                 if($db->getRowCount() == 0) {
                     $hash_password = password_hash($password, PASSWORD_BCRYPT);
-                    $result = $db->execute("INSERT INTO generaluser (username, email, password, name, phonenumber, gender, bdate, university, major, stdId ,stdId_img) VALUES ( ?,?,?,?,?,?,?,?,?,?,?)",array($username,$email,$hash_password,$name,$phonenumber,$gender,$bdate,$university,$major,$stdId,$stdId_img));
+                    $result = $db->execute("INSERT INTO generaluser (username, email, password, name, phonenumber, gender, bdate, university, major, stdId ,stdId_img) VALUES ( ?,?,?,?,?,?,?,?,?,?,?)",array($username,$email,$hash_password,$name,$phonenumber,$gender,$bdate,$university,$major,$stdId,$file));
                     echo $db->getErrorMessage();
                     if($db->getRowCount()) {
                       echo "註冊成功";
-                   } else { 
-                     echo "註冊失敗";
-                   } 
+                   } else {
+                      echo "註冊失敗";
+                   }
               } else {
                 echo "<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."該信箱已被註冊過!"."\"".")".";"."</script>";
                 echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";
-                exit;  
+                exit;
+              }
+              } else {
+                echo '不允許該檔案格式';
               }
              } else {
                echo "<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."姓名不可有特殊字元或英文"."\"".")".";"."</script>";
                echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";
-               exit;   
+               exit;
             }
           } else {
             echo "<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."請填寫正確性別"."\"".")".";"."</script>";
             echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";
             exit;
-           }  
+           }
           } else {
             echo "<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."學號錯誤"."\"".")".";"."</script>";
             echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";
@@ -68,16 +71,16 @@
           echo "<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."信箱格式錯誤"."\"".")".";"."</script>";
           echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";
           exit;
-        } 
+        }
      } else {
          echo "<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."手機格式錯誤"."\"".")".";"."</script>";
          echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";
-         exit; 
+         exit;
        }
   } else {
      echo "<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."密碼格式錯誤"."\"".")".";"."</script>";
-     echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";     
+     echo "<script type="."\""."text/javascript"."\"".">"."window.location="."\""."http://localhost/eric12345566/src/user/register.php"."\""."</script>";
      exit;
   }
-  
+
 ?>
--- src/user/register.php
+++ src/user/register.php
@@ -3,6 +3,7 @@
     <title>
       使用者註冊
     </title>
+    <script type="text/javascript" src="../user/checkpwd.js"></script>
   </head>
   <body>
   <div>
@@ -11,7 +12,8 @@
             <p><label>username</label><input type="text" name="username"  required></p>
             <p><label>Email</label><input type="email" name="email"  required></p>
             <p><label>姓名</label><input type="text" name="name" required></p>
-            <p><label>密碼</label><input type="password" name="password"  required></p>
+            <p><label>密碼</label><input type="password" id="password" name="password"  required></p>
+            <p><label>確認密碼</label><input type="password" id="passwordcheck" required></p>
             <p><label>手機</label><input type="tel" name="phonenumber" required></p>
             性別<select name="gender">
             <option value="男">男</option>
@@ -27,9 +29,9 @@
 　          <option value="電機系">電機系</option>
             </select>  
             <p><label>學號</label><input type="text" name="stdId" required></p>
-            <form action = register-api.php method = post enctype = multipart / form-data>
+            <form action = register-api.php method = post enctype = multipart/form-data>
             學生證上傳<br><input type=file name=stdId_img > <br>
-            <input type="submit"  value="確認" />
+            <input type="submit"  value="確認" onclick="login();"/>
             </form>
                 <div>
                 </div>
