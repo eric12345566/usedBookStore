09446364ba9c91911964176b6a945cc5c9ceda72
Eric Shih <eric12345566@gmail.com>
2020-06-04 17:38:29 +0800

    login api 修改中

--- src/backBoard/API/login-api.php
+++ src/backBoard/API/login-api.php
@@ -10,5 +10,8 @@
   $result = $db->execute("SELECT * from Admin Where Admin_name = ? AND password = ?", array($username, $password));
 
   if ($db->getRowCount()) {
-      header("Location: ");
+      // header("Location: http://" . Server::serverUrl . "/backboard/index.php");
+      echo "登入成功";
+  } else {
+      echo "登入失敗";
   }
