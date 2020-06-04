60d397bc165b389a369b58b522a1832691230b44
Steven <qazzaqqaz71@mail.fcu.edu.tw>
2020-06-04 22:22:11 +0800

    緩慢進行中

--- src/config/MySQL.php
+++ src/config/MySQL.php
@@ -2,11 +2,10 @@
     // mysql user: dbkid passwword: V8Yb2ps8a4Hnka6R
     // This file should be secure!
     // mysql config
-    class MySQL {
+    class MySQL
+    {
         const ADDRESS = '127.0.0.1';
         const USERNAME = "root";
         const PASSWORD = "";
         const DATABASE = "Practice";
     }
-
-?>
--- src/user/checkpassword.php
+++ src/user/checkpassword.php
@@ -5,9 +5,14 @@
   $password = $_POST["pass"];
   $check = $_POST['checkpass'];
   echo $_SESSION["token"];
-  if (!strcmp($password, $check)) {
-      $result = $db->execute("UPDATE USERNAME SET password = ? WHERE email = (SELECT usermail FROM UserEP WHERE link_no = ?) ;", array($password, $_SESSION["token"]));
-      if ($db->getRowCount()) {
+  if (!strcmp($password, $check)) {  //比較密碼與確認密碼有無被取用
+      $result = $db->execute("UPDATE USERNAME SET password = ?
+      WHERE email = (SELECT usermail FROM UserEP WHERE link_no = ?) ;", array($password, $_SESSION["token"]));
+
+      $result = $db->execute("UPDATE UserEP SET orded = ? WHERE token = ?;", array(1, $_SESSION["token"]));
+
+      if ($db->getRowCount()) { //如果
+          
           echo "更新成功";
       } else {
           echo $db->getErrorMessage();
