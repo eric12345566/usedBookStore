3ff0a35243833b7be1eea7ff1ae925994e64fe22
Steven <qazzaqqaz71@mail.fcu.edu.tw>
2020-06-10 21:04:31 +0800

    update

--- src/user/login-api.php
+++ src/user/login-api.php
@@ -1,6 +1,6 @@
 <?php
   require __DIR__ . '/../vendor/autoload.php';
-  
+
   // 建立 Session
   session_start();
 
@@ -14,17 +14,19 @@
   // 查詢資料庫
 
   // TODO: 針對使用者POST的資料做安全性過濾，再傳入 SQL 裡 query
-  
-  //有安全疑慮的寫法 $result = $db->execute("SELECT * FROM user WHERE username = " . "'" . $username. "'" . " AND password = " . "'" . $password . "';");
-  $result = $db->execute("SELECT * FROM user WHERE username = ? AND password = ?;", array($username, $password));
-  if($db->getRowCount()){
-    echo "歡迎登入，". $result[0]['username'];
-    
-    // username 註冊到 session 變數
-    $_SESSION['username'] = $result[0]['username'];
-  }else{
-    echo "登入失敗";
-  }
 
-  
-?>
+  //有安全疑慮的寫法 $result = $db->execute("SELECT * FROM user WHERE username = " . "'" . $username. "'" . " AND password = " . "'" . $password . "';");
+  $result = $db->execute("SELECT * FROM GeneralUser WHERE username = ? AND password = ?;", array($username, $password));
+  if ($db->getRowCount()) {
+      if (!strcmp(sha1($password), sha1($result[0]['password']))) {
+          echo "歡迎登入，". $username;
+          // username 註冊到 session 變數?>
+          <script type="text/javascript">window.location.href="homepage.php"</script>; //重新導向
+          <?php
+          $_SESSION['username'] = $result[0]['username'];
+      }
+  } else {
+      echo '<script type="text/javascript">alert("登入失敗!請確認帳號或密碼");</script>'; ?>
+<script type="text/javascript">window.location.href="login.php"</script>; //重新導向
+<?php
+  }
