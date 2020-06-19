7fb826756f0deee34acd0c86112decb4cac7eb70
Eric Shih <eric12345566@gmail.com>
2020-06-19 23:06:45 +0800

    完成訂單管理頁面

--- src/backBoard/API/order/order-update.php
+++ src/backBoard/API/order/order-update.php
@@ -6,29 +6,23 @@
   }
 
 
-  $product_no = $_POST['product_no'];
-  $book_name = $_POST['book_name'];
-  $ISBN = $_POST['ISBN'];
-  $publisher = $_POST['publisher'];
-  $publish_date = $_POST['publish_date'];
-  $price = $_POST['price'];
-  $avialiable = $_POST['avialiable'];
-  $b_language = $_POST['b_language'];
-  $exterior = $_POST['exterior'];
-  $stock = $_POST['stock'];
-  $author = $_POST['author'];
-  $introduce = $_POST['introduce'];
+    $order_no = $_POST['order_no'];
+    $order_status = $_POST['order_status'];
 
-  $isError = false;
+    $isError = false;
 
   $db = Database::get();
   if ($isError) {
-      header("Location: ../../product.php?errorupdatemsg=" . $error_msg);
+      header("Location: ../../order.php?errorupdatemsg=" . $error_msg);
   } else {
-      $result =  $db->execute("UPDATE book_product SET book_name=?, ISBN=?, publisher=?, publish_date=?, price=?, avialiable=?, b_language=?, exterior=?, stock=?, author=?, introduce=? WHERE product_no=?", array($book_name, $ISBN, $publisher, $publish_date, $price, $avialiable, $b_language, $exterior, $stock, $author, $introduce, $product_no));
-      // echo $db->getRowCount();
-      // echo $db->getErrorMessage();
-      if ($db->getRowCount()) {
-          header("Location: ../../product.php");
+      $result =  $db->execute("UPDATE p_order SET order_status=? WHERE order_no=?", array($order_status, $order_no));
+      
+      if ($db->getRowCount() > 0) {
+          header("Location: ../../order.php");
+      }else{
+        echo $db->getRowCount();
+        echo $db->getErrorMessage();
+        echo "出事情了啊杯";
+        die();
       }
   }
