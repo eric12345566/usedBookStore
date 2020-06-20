f87bdeca232f267507256a8906b2371db51b44b7
Steven <qazzaqqaz71@mail.fcu.edu.tw>
2020-06-20 13:42:39 +0800

    Yes成功了

--- src/user/search.php
+++ src/user/search.php
@@ -60,7 +60,6 @@
     $amount = $count[0]["C"];
     if ($amount == 0) {   //如果沒有搜尋結果
         echo "沒有搜尋結果 請輸入關鍵字搜尋!";
-        echo "拜託給我看一下啦";
     } else {
         $page_cnt = ceil($amount/$page_size);//紀錄頁面有幾個
         $page_num = empty($_GET['page'])?1:$_GET['page'];
